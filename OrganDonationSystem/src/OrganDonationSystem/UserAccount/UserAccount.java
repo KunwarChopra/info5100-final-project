4e8d0a67f787e332dbedb53c00f29d5019e09971
SourabhTripathi21 <Tripathi.S@northeastern.edu>
2021-12-07 00:41:54 -0500

    Added user account creation

--- OrganDonationSystem/src/OrganDonationSystem/UserAccount/UserAccount.java
+++ OrganDonationSystem/src/OrganDonationSystem/UserAccount/UserAccount.java
@@ -3,11 +3,65 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package OrganDonationSystem.UserAccount;
-
+import OrganDonationSystem.Individual.Individual;
+import OrganDonationSystem.Role.Role;
 /**
  *
  * @author saura
  */
 public class UserAccount {
-    
+    private String name;
+    private String username;
+    private String password;
+    private Individual individual;
+    private Role role;
+//    private WorkQueue workQueue;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+//    public UserAccount() {
+//
+//    }
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setPerson(Individual person) {
+        this.individual = individual;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public Individual getIndividual() {
+        return individual;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
--- OrganDonationSystem/src/OrganDonationSystem/UserAccount/UserAccountDirectory.java
+++ OrganDonationSystem/src/OrganDonationSystem/UserAccount/UserAccountDirectory.java
@@ -4,10 +4,60 @@
  */
 package OrganDonationSystem.UserAccount;
 
+import OrganDonationSystem.Individual.Individual;
+import OrganDonationSystem.Role.Role;
+import java.util.ArrayList;
+
 /**
  *
  * @author saura
  */
 public class UserAccountDirectory {
+    private ArrayList<UserAccount> userAccountList;
+
+    public UserAccountDirectory() {
+        userAccountList = new ArrayList();
+    }
+
+    public ArrayList<UserAccount> getUserAccountList() {
+        return userAccountList;
+    }
     
+    public UserAccount authenticateUser(String username, String password){
+        for (UserAccount ua : userAccountList)
+            if (ua.getUsername().equals(username) && ua.getPassword().equals(password)){
+                return ua;
+            }
+        return null;
+    }
+    
+    public UserAccount createUserAccount(String name, String username, String password, Individual individual, Role role){
+        UserAccount userAccount = new UserAccount();
+        userAccount.setName(name);
+        userAccount.setUsername(username);
+        userAccount.setPassword(password);
+        userAccount.setPerson(individual);
+        userAccount.setRole(role);
+        userAccountList.add(userAccount);
+        return userAccount;
+    }
+    
+    public boolean checkIfUsernameIsUnique(String username){
+        for (UserAccount ua : userAccountList){
+            if (ua.getUsername().equals(username))
+                return false;
+        }
+        return true;
+    }
+    
+    public void updateUserAccount(UserAccount user,String name,String username, String password){
+       
+        user.setName(name);
+        user.setUsername(username);
+        user.setPassword(password);
+    }
+    
+    public void deleteUserAccount(UserAccount user){
+        userAccountList.remove(user);
+    }
 }
